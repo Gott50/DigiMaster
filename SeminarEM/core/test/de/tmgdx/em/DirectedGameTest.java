996064849c8d262bb5feb1f041ff9175cba565ff
Gott50 <xxGott50xx@gmail.com>
2016-04-14 21:15:26 +0200

    add UnitTest
    a first attempt, they have to be overwritten.

---
+++ SeminarEM/core/test/de/tmgdx/em/DirectedGameTest.java
@@ -0,0 +1,71 @@
+package de.tmgdx.em;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DirectedGameTest {
+	private DirectedGame game;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+//		game = new DirectedGame();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testCreate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDispose() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testPause() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testRender() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testResize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testResume() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetScreenAbstractScreen() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetScreenAbstractScreenScreenTransition() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
